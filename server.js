6b9e61d2583bfb30624ad66e5fb70346597ae1fb
DESKTOP-EB3BLCO\Admin <yeapman@ukr.net>
2017-03-17 15:46:04 +0200

    lal

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const app = express();
 const myRouter = express.Router();
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+const mongoose = require('mongoose');
+// var db = mongoose.connect('mongodb://localhost/27017/client');
+var url = 'mongodb://localhost:27017/client';
+var Book = require('./src/app/models/bookeModel');
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
@@ -23,24 +32,54 @@ app.use(function (req, res, next) {
   // Pass to next layer of middleware
   next();
 });
-myRouter.route('/Books')
-  .get(function(req, res){
-    var responseJson =
-    {
-      "data": [
-      {"first_name": "Yarik",
-      "second_name": "fonarik"
-      }
-      ]
-    };
-
-    res.json(responseJson);
-  });
 app.use('/api', myRouter);
 
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("connected successfull to server");
+
+  insertDocuments(db, function() {
+    findDocuments(db, function() {
+      db.close();
+    });
+  });
+});
+var insertDocuments = function(db, callback) {
+  var collection = db.collection('documents');
+  collection.insertMany([
+    {'first_name': 'yarik'}
+  ], function(err, result) {
+
+    console.log("inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+var findDocuments = function(db, callback) {
+  var collection = db.collection('documents');
+
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("found this document");
+    console.log(docs);
+    callback(docs);
+  })
+}
+
+myRouter.route('/Books')
+  // .post(function(req, res) {
+  //   var book = new Book(req.body);
+  //   console.log(book);
+  //   res.send(book);
+  // })
+  .get(function(req, res){
+    Book.find(function(err, book ){
+      if(err)
+        console.log(err)
+      else
+        res.json(docs)
+    })
+  });
+
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
