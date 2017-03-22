21173bf4b2c46c7786d31901e6a797da1c674c38
DESKTOP-EB3BLCO\Admin <yeapman@ukr.net>
2017-03-22 14:13:13 +0200

    can delete from database by remove

--- server/libs/api.js
+++ server/libs/api.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 var url = 'mongodb://localhost:27017/client';
+var ObjectId = require('mongodb').ObjectID;
 // middleware that is specific to this router
 
 
@@ -23,26 +24,42 @@ MongoClient.connect(url, function(err, db) {
   // assert.equal(null, err);
   console.log("connected successfull to server");
 
-  // insertDocuments(db, function() {
-  //   findDocuments(db, function() {
-  //     db.close();
-  //   });
-  // });
 
   findDocuments(db, function() {
     db.close();
   });
+
+  // deleteDocument(db, function() {
+  //   db.close();
+  // });
+
+
+
+  // findData(db, function() {
+  //   db.close();
+  // });
+
+// insertDocuments(db, function() {
+//   db.close();
+// });
+
 });
-// var insertDocuments = function(db, callback) {
-//   var collection = db.collection('levindb');
-//   collection.insertMany([
-//     {'first_name': 'yarik'}
-//   ], function (err, result) {
-//
-//     console.log("inserted 1 documents into the collection");
-//     callback(result);
-//   });
-// }
+var insertDocuments = function(db, callback) {
+  var collection = db.collection('levindb');
+  collection.insertMany([
+    { "data2":
+      {
+        'first_name': 'yarik',
+        'last_name': 'levin',
+        'age': 22
+    }
+    }
+  ], function (err, result) {
+
+    console.log("inserted 1 documents into the collection");
+    callback(result);
+  });
+}
 
 var findDocuments = function(db, callback) {
   var collection = db.collection('levindb');
@@ -53,6 +70,24 @@ var findDocuments = function(db, callback) {
     console.log(docs);
     callback(docs);
   });
+}
+
+// var findData = function(db, callback) {
+//   var collection = db.collection('levindb');
+//
+//   collection.findOne({_id: '58d2461eaec9921d686fe2c4'}, function(err, document) {
+//     assert.equal(err, null);
+//     console.log(document.first_name);
+//     callback(document);
+//   });
+// }
+var deleteDocument = function(db, callback) {
+  var collection = db.collection('levindb');
+
+    collection.remove( { "_id" : ObjectId("58d2492a6a33d81c3cbc61fc")}, function(err, result) {
+      console.log("delete one line");
+      callback(result);
+    });
 }
 
 
